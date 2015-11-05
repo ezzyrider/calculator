035b27a353e66a6241b7167c6d0962a138141b6a
ezzyrider <benjamib@me.com>
2015-11-05 14:10:08 -0500

    Update calculator.rb

--- calculator.rb
+++ calculator.rb
@@ -1,17 +1,30 @@
-puts "enter the first number"
+def say(msg)
+  puts "=> #{msg}"
+end
+
+
+say "enter the first number"
 num1 = gets.chomp
-puts "enter the second number"
+say "enter the second number"
 num2 = gets.chomp
 
-puts "Enter 1) for addition, 2) for subtraction, 3) for multiplication, or 4) for division"
+say "Enter 1) for addition, 2) for subtraction, 3) for multiplication, or 4) for division"
 sign = gets.chomp
 
 if sign == '1'
-	puts "#{num1} plus #{num2} = #{num1.to_i + num2.to_i} "
+  result = num1.to_i + num2.to_i
+  operator = "plus"
 elsif sign == '2'
-	puts "#{num1} minus #{num2} = #{num1.to_i - num2.to_i} " 
+  result = num1.to_i - num2.to_i
+  operator = "minus"
 elsif sign == '3'
-	puts "#{num1} times #{num2} = #{num1.to_i * num2.to_i} "
+  result = num1.to_i * num2.to_i
+  operator = "times"
 else
-	puts "#{num1} divided by #{num2} = #{num1.to_f / num2.to_f} "
+  result = num1.to_f / num2.to_f
+  operator = "divided by"
 end
+
+puts "-----------------------------"
+puts "#{num1} #{operator} #{num2} is #{result}"
+puts "-----------------------------"
